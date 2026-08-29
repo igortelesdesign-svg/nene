e19aecdcfafdfaf177e08b8acf2d3f7d98661ca7
igor_teles <igortelesdesign@gmail.com>
2026-08-29 10:41:52 -0300

    feat: adiciona versao iOS com Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.byigorteles.nene',
+  appName: ' NENÊ',
+  webDir: 'dist'
+};
+
+export default config;
