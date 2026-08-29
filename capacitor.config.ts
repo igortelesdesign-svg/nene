3ea10369b05a68449e88637e7f236025791b3503
igor_teles <igortelesdesign@gmail.com>
2026-08-29 11:28:13 -0300

    feat: configura identidade do app iOS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.byigorteles.nene',
-  appName: ' NENÊ',
+  appName: 'NENÊ',
   webDir: 'dist'
 };
 
